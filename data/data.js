0522013cac3727d62aa9e848e420b150b5afe630
vagabondms <hiseokseok@gmail.com>
2021-12-06 23:38:49 +0900

    zz

--- data/data.js
+++ data/data.js
@@ -1,15 +1,24 @@
 class Outfit {
-  constructor(imgSrc, sex, size) {
-    this.imgSrc = `src/${imgSrc}`;
-    this.sex = sex;
-    this.size = `${size} size`;
+  constructor(imgSrc) {
+    this.imgSrc = `img/${imgSrc}`;
+    this.gender = Outfit.genderGenerator;
+    this.size = `${Outfit.sizeGenerator} size`;
   }
+
+  static randomNumberGenerator = () => Math.random;
+  static genderGenerator =
+    this.randomNumberGenerator() > 0.5 ? "male" : "female";
+  static sizeGenerator = this.randomNumberGenerator() > 0.5 ? "big" : "small";
 }
 
-const randomNumberGenerator = () => Math.random;
-const sexGenerator = randomNumberGenerator() > 0.5 ? "male" : "female";
-const sizeGenerator = randomNumberGenerator() > 0.5 ? "big" : "small";
+const colors = ["blue", "pink", "yellow"];
+const types = ["p", "s", "t"];
 
-const outfits = [new Outfit()];
+const outfits = colors.reduce((acc, cur) => {
+  const colorTypePair = types.map((type) => {
+    return new Outfit(`${cur}_${type}.png`);
+  });
+  return [...acc, ...colorTypePair];
+}, []);
 
-export default outfits;
+export default [...outfits, ...outfits];
--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-import data from "../data/data.js";
+import outfits from "../data/data.js";
+
+const outfitDiv = document.querySelector("#outfits");
+
+const wrapper = document.createElement("ul");
+outfits.forEach((outfit) => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  img.src = outfit.imgSrc;
+  const details = document.createElement("div");
+  details.innerHTML = `${outfit.gender} ${outfit.size}`;
+  li.appendChild(img);
+  li.appendChild(details);
+  wrapper.appendChild(li);
+});
+
+outfitDiv.appendChild(wrapper);
